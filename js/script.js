66e3b20baea201a57f0c1d833b0f35b0f155f467
Iskander Dauletov <iskanderdauletov@gmail.com>
2023-07-21 15:35:33 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+selectTag = document.querySelectorAll("select"),
+exchangeIcon = document.querySelector(".exchange"),
+translateBtn = document.querySelector("button"),
+icons = document.querySelectorAll(".row i");
+
+selectTag.forEach((tag, id) => {
+    for(const country_code in countries){
+        // console.log(countries[country_code] + ": " + country_code);
+        let selected;
+        if(id == 0 && country_code == "en-GB"){
+            selected = "selected";
+        } else if(id == 1 && country_code == "ru-RU"){
+            selected = "selected";
+        }
+        let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+})
+
+exchangeIcon.addEventListener("click", () => {
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    toText.value = tempText;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang;
+    
+})
+
+function translate(){
+    let text = fromText.value,
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    // console.log(text, translateFrom, translateTo);
+    if(!text) return;
+    toText.setAttribute("placeholder", "Translating...")
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        // console.log(data.responseData.translatedText);
+        toText.value = data.responseData.translatedText;
+        toText.setAttribute("placeholder", "Translation")
+    })
+}
+
+window.addEventListener("keyup", e =>{
+    if(e.key == "Enter"){
+        translate();
+    }
+})  
+
+translateBtn.addEventListener("click", translate
+    // let text = fromText.value,
+    // translateFrom = selectTag[0].value,
+    // translateTo = selectTag[1].value;
+    // // console.log(text, translateFrom, translateTo);
+    // if(!text) return;
+    // toText.setAttribute("placeholder", "Translating...")
+    // let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    // fetch(apiUrl).then(res => res.json()).then(data => {
+    //     // console.log(data.responseData.translatedText);
+    //     toText.value = data.responseData.translatedText;
+    //     toText.setAttribute("placeholder", "Translation")
+    // })
+)
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+        // console.log(target);
+        if(target.classList.contains("fa-copy")){
+            if(target.id == "from"){
+                navigator.clipboard.writeText(fromText.value)
+            } else {
+                navigator.clipboard.writeText(toText.value)
+            }
+        } else {
+            let utterance;
+            if(target.id == "from"){
+                utterance = new SpeechSynthesisUtterance(fromText.value);
+                utterance.lang = selectTag[0].value;
+            } else {
+                utterance = new SpeechSynthesisUtterance(toText.value);
+                utterance.lang = selectTag[1].value;
+            }
+            speechSynthesis.speak(utterance)
+        }
+    })
+})
